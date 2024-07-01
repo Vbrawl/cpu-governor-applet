01c01391ee41fb139ccee480cb6ea8ee87392caf
VBrawl <konstantosjim@gmail.com>
2024-07-01 07:37:19 +0300

    Extracted backend logic from the front-end / main applet code.

--- cpu-governor@vbrawl/applet.js
+++ cpu-governor@vbrawl/applet.js
@@ -9,6 +9,60 @@ const SignalManager = imports.misc.signalManager;
 const CPU_FREQUENCY_PATH = "/sys/devices/system/cpu/cpufreq/";
 
 
+const CPUGovernorManager = class CPUGovernorManager {
+    constructor(callback) {
+        this._cpu_governor_paths = [];
+        const cpu_governor_dir = Gio.File.new_for_path(CPU_FREQUENCY_PATH);
+        FileUtils.listDirAsync(cpu_governor_dir, (paths) => {
+            for (let i = 0; i < paths.length; i++) {
+                const path = paths[i];
+                this._cpu_governor_paths.push(GLib.build_filenamev([CPU_FREQUENCY_PATH, path.get_name()]));
+            }
+            
+            this._cpu_governor_paths.sort();
+            callback();
+        });
+    }
+
+    get_governor() {
+        return String(GLib.file_get_contents(GLib.build_filenamev([this._cpu_governor_paths[0], "scaling_governor"]))[1]).trim();
+    }
+
+    set_governor(governor) {
+        for (let i = 0; i < this._cpu_governor_paths.length; i++) {
+            try {
+                const path = GLib.build_filenamev([this._cpu_governor_paths[i], "scaling_governor"]);
+                GLib.file_set_contents_full(path, governor, GLib.FileSetContentsFlags.NONE, 0o0660);
+            }
+            catch(e) { global.log(e); }
+        }
+    }
+
+    get_available_governors() {
+        var available_governors = [];
+        for (let i = 0; i < this._cpu_governor_paths.length; i++) {
+            const path = GLib.build_filenamev([this._cpu_governor_paths[i], "scaling_available_governors"]);
+            const governors = String(GLib.file_get_contents(path)[1]).trim().split(' ');
+
+            if(i === 0) {
+                available_governors = governors;
+            }
+            else {
+                for (let g_i = 0; g_i < available_governors.length; g_i++) {
+                    const available_governor = available_governors[g_i];
+                    if(governors.findIndex((v, i, o) => {return v === available_governor;}) === -1) {
+                        available_governor.remove(available_governor);
+                        g_i-=1;
+                    }
+                }
+            }
+        }
+        return available_governors;
+    }
+}
+
+
+
 const GovernorSection = class GovernorSection extends PopupMenu.PopupMenuSection {
     constructor(owner, available_governors, on_option_click) {
         super();
@@ -45,69 +99,23 @@ const CPUGovernor = class CPUGovernor extends Applet.IconApplet {
         this.set_applet_icon_name("logo");
         this.set_applet_tooltip("Change CPU Governor");
 
-
-        this._cpu_governor_paths = [];
-        const cpu_governor_dir = Gio.File.new_for_path(CPU_FREQUENCY_PATH);
-        FileUtils.listDirAsync(cpu_governor_dir, (paths) => {
-            for (let i = 0; i < paths.length; i++) {
-                const path = paths[i];
-                this._cpu_governor_paths.push(GLib.build_filenamev([CPU_FREQUENCY_PATH, path.get_name()]));
-            }
-
-            this._cpu_governor_paths.sort();
-            this.available_governors = this.get_available_governors();
-
+        this._governor_manager = new CPUGovernorManager(() => {
+            this.available_governors = this._governor_manager.get_available_governors();
 
             /********************
              * Setup Popup Menu *
              ********************/
             this._menu_manager = new PopupMenu.PopupMenuManager(this);
             this._menu = new Applet.AppletPopupMenu(this, orientation);
-            this._menu_governor_section = new GovernorSection(this, this.available_governors, this.set_governor);
+            this._menu_governor_section = new GovernorSection(this, this.available_governors, this.update_governor);
             this._menu.addMenuItem(this._menu_governor_section);
             this._menu_manager.addMenu(this._menu);
         });
 
     }
 
-    get_governor() {
-        return String(GLib.file_get_contents(GLib.build_filenamev([this._cpu_governor_paths[0], "scaling_governor"]))[1]).trim();
-    }
-
-    set_governor(_this, governor) {
-        for (let i = 0; i < _this._cpu_governor_paths.length; i++) {
-            try {
-                const path = GLib.build_filenamev([_this._cpu_governor_paths[i], "scaling_governor"]);
-                GLib.file_set_contents_full(path, governor, GLib.FileSetContentsFlags.NONE, 0o0660);
-            }
-            catch(e) { global.log(e); }
-        }
-    }
-
-    get_available_governors() {
-        var available_governors = [];
-        for (let i = 0; i < this._cpu_governor_paths.length; i++) {
-            const path = GLib.build_filenamev([this._cpu_governor_paths[i], "scaling_available_governors"]);
-            const governors = String(GLib.file_get_contents(path)[1]).trim().split(' ');
-
-            if(i === 0) {
-                available_governors = governors;
-            }
-            else {
-                for (let g_i = 0; g_i < available_governors.length; g_i++) {
-                    const available_governor = available_governors[g_i];
-                    if(governors.findIndex((v, i, o) => {return v === available_governor;}) === -1) {
-                        available_governor.remove(available_governor);
-                        g_i-=1;
-                    }
-                }
-            }
-        }
-        return available_governors;
-    }
-
     check_governor_update() {
-        const governor = this.get_governor();
+        const governor = this._governor_manager.get_governor();
         this._menu_governor_section.update_governor_label(governor);
         return governor;
     }
@@ -115,6 +123,10 @@ const CPUGovernor = class CPUGovernor extends Applet.IconApplet {
     on_applet_clicked() {
         this._menu.toggle();
         this.check_governor_update();
+    }
+
+    update_governor(_this, governor) {
+        _this._governor_manager.set_governor(governor);
     }
 }
 
